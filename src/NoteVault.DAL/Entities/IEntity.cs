60ba3d5b67abe03016aaa46a0b34c205f2d9e95c
kirill <kirillchizh2411@gmail.com>
2026-06-07 23:20:02 +0300

    feat(dal): add base note models

---
+++ src/NoteVault.DAL/Entities/IEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace NoteVault.DAL.Entities
+{
+    public interface IEntity
+    {
+        Guid Id { get; set; }
+    }
+}
---
+++ src/NoteVault.DAL/Entities/Note.cs
@@ -0,0 +1,12 @@
+﻿namespace NoteVault.DAL.Entities
+{
+    public class Note : IEntity
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public List<Tag> Tags { get; set; } = new();
+        public List<string> ImageUrls { get; set; } = new();
+        public DateTime CreationDate { get; set; }
+    }
+}
