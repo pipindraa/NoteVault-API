ad141b916fa41545f536263292d8bcb3d2714aed
kirill <kirillchizh2411@gmail.com>
2026-06-08 21:46:40 +0300

    feat(bll): add notes dto

---
+++ src/NoteVault.BLL/DTOs/Notes/NoteResponseDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NoteVault.BLL.DTOs.Notes
+{
+    public class NoteResponseDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public List<string> ImageUrls { get; set; } = new();
+        public DateTime CreationDate { get; set; }
+    }
+}
