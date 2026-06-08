d6d0e710e78bcd25833ebd59ed9107843392a099
kirill <kirillchizh2411@gmail.com>
2026-06-08 22:47:34 +0300

    refactor: resolve dry

--- src/NoteVault-API/Program.cs
+++ src/NoteVault-API/Program.cs
@@ -9,22 +9,19 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<AppDbContext>(options => 
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwagger(); // extention
+    app.UseSwaggerUI(); // extention
 }
 
-app.UseHttpsRedirection(); 
+app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-app.MapControllers();
+app.MapControllers(); // extention
 
 app.Run();
