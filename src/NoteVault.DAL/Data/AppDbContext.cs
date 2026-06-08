93c99035d0e94fb45c947dff2d364d07a7d07bec
kirill <kirillchizh2411@gmail.com>
2026-06-08 22:12:54 +0300

    refactor(dal): moved properties before constructor

--- src/NoteVault.DAL/Data/AppDbContext.cs
+++ src/NoteVault.DAL/Data/AppDbContext.cs
@@ -5,11 +5,11 @@ namespace NoteVault.DAL.Data
 {
     public class AppDbContext : DbContext
     {
+        public DbSet<Note> Notes { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
         {
         }
-
-        public DbSet<Note> Notes { get; set; }
-        public DbSet<Tag> Tags { get; set; }
     }
 }
