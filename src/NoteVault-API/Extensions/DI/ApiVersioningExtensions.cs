01a007af29dc773f026ee2723360a074c3a6b91c
kirill <kirillchizh2411@gmail.com>
2026-06-09 15:07:19 +0300

    feat(api): add api versioning support

---
+++ src/NoteVault-API/Extensions/DI/ApiVersioningExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Asp.Versioning;
+
+namespace NoteVault_API.Extensions.DI
+{
+    public static class ApiVersioningExtensions
+    {
+        public static IServiceCollection AddApiVersioningSupport(this IServiceCollection services)
+        {
+            services
+                .AddApiVersioning(options =>
+                {
+                    options.DefaultApiVersion = new ApiVersion(1, 0);
+                    options.AssumeDefaultVersionWhenUnspecified = true;
+                    options.ReportApiVersions = true;
+                    options.ApiVersionReader = new UrlSegmentApiVersionReader();
+                })
+                .AddApiExplorer(options =>
+                {
+                    options.GroupNameFormat = "'v'VVV";
+                    options.SubstituteApiVersionInUrl = true;
+                });
+
+            return services;
+        }
+    }
+}
