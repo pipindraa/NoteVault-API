a528d8ec21fbee483c11c2dfbbc71bf5cac94dfd
kirill <kirillchizh2411@gmail.com>
2026-06-09 15:28:39 +0300

    feat(bll): add note service contract

---
+++ src/NoteVault.BLL/Interfaces/INoteService.cs
@@ -0,0 +1,13 @@
+﻿using NoteVault.BLL.DTOs.Notes;
+
+namespace NoteVault.BLL.Interfaces
+{
+    public interface INoteService
+    {
+        Task<IReadOnlyCollection<NoteResponseDto>> GetAllAsync(CancellationToken cancellationToken = default);
+        Task<NoteResponseDto?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+        Task<NoteResponseDto> CreateAsync(NoteCreateDto request, CancellationToken cancellationToken = default);
+        Task<NoteResponseDto?> UpdateAsync(Guid id, NoteUpdateDto request, CancellationToken cancellationToken = default);
+        Task<bool> DeleteAsync(Guid id, CancellationToken cancellationToken = default);
+    }
+}
