2213876ba8677ce8bae5296a1aa520b8c8cb7de5
kirill <kirillchizh2411@gmail.com>
2026-06-11 14:26:05 +0300

    refactor(api): replace magic values in constants

--- src/NoteVault-API/Constants/ApiVersions.cs
+++ src/NoteVault-API/Constants/ApiVersions.cs
@@ -3,5 +3,11 @@
     public static class ApiVersions
     {
         public const string V1 = "1.0";
+        public const int MajorVersion = 1;
+        public const int MinorVersion = 0;
+        public const string GroupNameFormat = "'v'VVV";
+        public const bool AssumeDefaultVersion = true;
+        public const bool ReportApiVersions = true;
+        public const bool SubstiteApiVersionInUrl = true;
     }
 }
--- src/NoteVault-API/Extensions/DI/ApiVersioningExtensions.cs
+++ src/NoteVault-API/Extensions/DI/ApiVersioningExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Asp.Versioning;
+using NoteVault_API.Constants;
 
 namespace NoteVault_API.Extensions.DI
 {
@@ -9,15 +10,15 @@ namespace NoteVault_API.Extensions.DI
             services
                 .AddApiVersioning(options =>
                 {
-                    options.DefaultApiVersion = new ApiVersion(1, 0);
-                    options.AssumeDefaultVersionWhenUnspecified = true;
-                    options.ReportApiVersions = true;
+                    options.DefaultApiVersion = new ApiVersion(ApiVersions.MajorVersion, ApiVersions.MinorVersion);
+                    options.AssumeDefaultVersionWhenUnspecified = ApiVersions.AssumeDefaultVersion;
+                    options.ReportApiVersions = ApiVersions.ReportApiVersions;
                     options.ApiVersionReader = new UrlSegmentApiVersionReader();
                 })
                 .AddApiExplorer(options =>
                 {
-                    options.GroupNameFormat = "'v'VVV";
-                    options.SubstituteApiVersionInUrl = true;
+                    options.GroupNameFormat = ApiVersions.GroupNameFormat;
+                    options.SubstituteApiVersionInUrl = ApiVersions.SubstiteApiVersionInUrl;
                 });
 
             return services;
