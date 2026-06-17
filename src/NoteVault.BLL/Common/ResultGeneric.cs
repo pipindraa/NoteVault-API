86945bc298dab964d9e28a84dcf949d42d7be6ce
kirill <kirillchizh2411@gmail.com>
2026-06-17 12:05:06 +0300

    refactor(bll): split Result and Result<T> in separate files

---
+++ src/NoteVault.BLL/Common/ResultGeneric.cs
@@ -0,0 +1,19 @@
+﻿namespace NoteVault.BLL.Common
+{
+    public class Result<T> : Result
+    {
+        private readonly T? _value;
+
+        public T Value => IsSuccess
+            ? _value!
+            : throw new InvalidOperationException("Cannot access the value of a failed result.");
+
+        private Result(T? value, bool isSuccess, string? errorMessage) : base(isSuccess, errorMessage)
+        {
+            _value = value;
+        }
+
+        public static Result<T> Success(T value) => new(value, true, null);
+        public static new Result<T> Failure(string errorMessage) => new(default, false, errorMessage);
+    }
+}
