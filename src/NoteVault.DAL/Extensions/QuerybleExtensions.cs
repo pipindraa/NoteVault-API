53cb9db47eb98898a341c2395665d633a1f5188a
kirill <kirillchizh2411@gmail.com>
2026-06-23 09:13:41 +0300

    refactor(dal): remove magic number in pagination logic

--- src/NoteVault.DAL/Extensions/QuerybleExtensions.cs
+++ src/NoteVault.DAL/Extensions/QuerybleExtensions.cs
@@ -1,13 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
 
 namespace NoteVault.DAL.Extensions
 {
     public static class QuerybleExtensions
     {
+        private const int FirstpageNumber = 1;
+
         public static async Task<List<T>> ToPagedListAsync<T>(this IQueryable<T> query, int  pageNumber, int pageSize, CancellationToken cancellationToken = default)
         {
             return await query
-                .Skip((pageNumber - 1) * pageSize)
+                .Skip((pageNumber - FirstpageNumber) * pageSize)
                 .Take(pageSize)
                 .ToListAsync(cancellationToken);
         }
