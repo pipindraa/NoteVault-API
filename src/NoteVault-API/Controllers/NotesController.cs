c4788ce41d1854975d80d4d23a7180c9d87b177b
kirill <kirillchizh2411@gmail.com>
2026-06-23 08:32:04 +0300

    refactor(api): remove controller dependency from ResultExstensions

--- src/NoteVault-API/Controllers/NotesController.cs
+++ src/NoteVault-API/Controllers/NotesController.cs
@@ -24,35 +24,35 @@ namespace NoteVault_API.Controllers
         public async Task<ActionResult<IReadOnlyCollection<NoteResponseDto>>> GetAll([FromQuery] PaginationRequest pagination, CancellationToken cancellationToken)
         {
             var result = await _noteService.GetAllAsync (pagination, cancellationToken);
-            return result.ToActionResult(this);
+            return result.ToActionResult();
         }
 
         [HttpGet(ApiRoutes.Notes.IdRoute)]
         public async Task<ActionResult<NoteResponseDto>> GetById([FromRoute] Guid id, CancellationToken cancellationToken)
         {
             var result = await _noteService.GetByIdAsync(id, cancellationToken);
-            return result.ToActionResult(this);
+            return result.ToActionResult();
         }
 
         [HttpPost]
         public async Task<ActionResult<NoteResponseDto>> Create([FromBody] NoteCreateDto request, CancellationToken cancellationToken)
         {
             var result = await _noteService.CreateAsync(request, cancellationToken);
-            return result.ToActionResult(this);
+            return result.ToActionResult();
         }
 
         [HttpPut(ApiRoutes.Notes.IdRoute)]
         public async Task<ActionResult<NoteResponseDto>> Update([FromRoute] Guid id, [FromBody] NoteUpdateDto request, CancellationToken cancellationToken)
         {
             var result = await _noteService.UpdateAsync(id, request, cancellationToken);
-            return result.ToActionResult(this);
+            return result.ToActionResult();
         }
 
         [HttpDelete(ApiRoutes.Notes.IdRoute)]
         public async Task<ActionResult> Delete([FromRoute] Guid id, CancellationToken cancellationToken)
         {
             var result = await _noteService.DeleteAsync(id, cancellationToken);
-            return result.ToActionResult(this);
+            return result.ToActionResult();
         }
     }
 }
--- src/NoteVault-API/Extensions/DI/ResultExtensions.cs
+++ src/NoteVault-API/Extensions/DI/ResultExtensions.cs
@@ -1,32 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using NoteVault.BLL.Common;
 using NoteVault_API.Models;
+using System.Reflection.Metadata.Ecma335;
 
 namespace NoteVault_API.Extensions.DI
 {
     public static class ResultExtensions
     {
-        public static ActionResult ToActionResult(this Result result, ControllerBase controller)
+        public static ActionResult ToActionResult(this Result result)
         {
             if (result.IsSuccess)
             {
-                return controller.NoContent();
+                return new NoContentResult();
             }
 
-            return ToErrorActionResult(result, controller);
+            return ToErrorActionResult(result);
         }
 
-        public static ActionResult<T> ToActionResult<T>(this Result<T> result, ControllerBase controller)
+        public static ActionResult<T> ToActionResult<T>(this Result<T> result)
         {
             if (result.IsSuccess)
             {
-                return controller.Ok(result.Value);
+                return new OkObjectResult(result.Value);
             }
 
-            return (ActionResult<T>)ToErrorActionResult(result, controller);
+            return (ActionResult<T>)ToErrorActionResult(result);
         }
 
-        private static ActionResult ToErrorActionResult(this Result result, ControllerBase controller)
+        private static ActionResult ToErrorActionResult(this Result result)
         {
             var primaryError = result.Errors.FirstOrDefault();
             int statusCode = GetHttpStatusCode(primaryError);
@@ -39,9 +40,9 @@ namespace NoteVault_API.Extensions.DI
 
             return statusCode switch
             {
-                StatusCodes.Status404NotFound => controller.NotFound(errorResponse),
-                StatusCodes.Status400BadRequest => controller.BadRequest(errorResponse),
-                _ => controller.StatusCode(statusCode, errorResponse)
+                StatusCodes.Status404NotFound => new NotFoundObjectResult(errorResponse),
+                StatusCodes.Status400BadRequest => new BadRequestObjectResult(errorResponse),
+                _ => new OkObjectResult(errorResponse) { StatusCode = statusCode }
             };
         }
 
