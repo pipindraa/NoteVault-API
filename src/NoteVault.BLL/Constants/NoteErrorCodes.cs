dd132bd40570cffa8abaac0a1a020c4103e75b33
kirill <kirillchizh2411@gmail.com>
2026-07-02 15:23:12 +0300

    refactor(bll): remove obsolete constants and clean up serrvice dependencies

--- src/NoteVault.BLL/Constants/NoteErrorCodes.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace NoteVault.BLL.Constants
-{
-    internal static class NoteErrorCodes
-    {
-        internal const string NotFound = "NOTE_NOT_FOUND";
-    }
-}
--- src/NoteVault.BLL/Constants/NoteErrorMessages.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace NoteVault.BLL.Constants
-{
-    internal static class NoteErrorMessages
-    {
-        internal const string NotFoundTemplate = "Note with id {0} was not found.";
-    }
-}
