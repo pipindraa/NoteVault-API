bdae44f7bafd6a8b7304290280f3b70cafadf62f
kirill <kirillchizh2411@gmail.com>
2026-07-02 12:25:43 +0300

    feat(bll): add error messages collection to Result and Result<T> structures

--- src/NoteVault.BLL/Common/Result.cs
+++ src/NoteVault.BLL/Common/Result.cs
@@ -1,23 +1,32 @@
-﻿using System.Linq;
-
-namespace NoteVault.BLL.Common
+﻿namespace NoteVault.BLL.Common
 {
     public class Result
     {
-        private static readonly Result _success = new(true, Array.Empty<ErrorCode>());
+        private static readonly Result _success = new(true, Array.Empty<ErrorCode>(), Enumerable.Empty<string>());
 
         public bool IsSuccess { get; }
         public bool IsFailure => !IsSuccess;
         public IReadOnlyCollection<ErrorCode> Errors { get; }
+        public IEnumerable<string> ErrorMessages { get; }
 
-        protected Result(bool isSuccess, IReadOnlyCollection<ErrorCode> errors)
+        protected Result(bool isSuccess, IReadOnlyCollection<ErrorCode> errors, IEnumerable<string> errorMessages)
         {
             IsSuccess = isSuccess;
             Errors = errors;
+            ErrorMessages = errorMessages;
         }
 
-        public static Result Success() => _success;
-        public static Result Failure(ErrorCode error) => new(false, new[] {error});
-        public static Result Failure(IReadOnlyCollection<ErrorCode> errors) => new(false, errors);
+        public static Result Success()
+        {  
+            return _success; 
+        }
+        public static Result Failure(ErrorCode error, IEnumerable<string>? messages = null)
+        {
+            return new Result(false, new[] { error }, messages ?? Enumerable.Empty<string>());
+        }
+        public static Result Failure(IReadOnlyCollection<ErrorCode> errors, IEnumerable<string>? messages = null)
+        {
+            return new Result(false, errors, messages ?? Enumerable.Empty<string>());
+        }
     }
 }
--- src/NoteVault.BLL/Common/ResultGeneric.cs
+++ src/NoteVault.BLL/Common/ResultGeneric.cs
@@ -4,28 +4,28 @@
     {
         public T Value { get;  }
 
-        private Result(T? value, bool isSuccess, IReadOnlyCollection<ErrorCode> errors) : base(isSuccess, errors)
+        private Result(T? value, bool isSuccess, IReadOnlyCollection<ErrorCode> errors, IEnumerable<string> errorMessages) : base(isSuccess, errors, errorMessages)
         {
             Value = value;
         }
 
-        private Result(bool isSuccess, IReadOnlyCollection<ErrorCode> errors) : base(isSuccess, errors)
+        private Result(bool isSuccess, IReadOnlyCollection<ErrorCode> errors, IEnumerable<string> errorMessages) : base(isSuccess, errors,  errorMessages)
         {
             Value = default;
         }
 
         public static Result<T> Success(T value)
         {
-            return new Result<T>(value, true, Array.Empty<ErrorCode>());
+            return new Result<T>(value, true, Array.Empty<ErrorCode>(), Enumerable.Empty<string>());
         }
-        public static Result<T> Failure(ErrorCode error)
+        public static Result<T> Failure(ErrorCode error, IEnumerable<string>? messages = null)
         {
-            return new Result<T> (false, new[] { error });
+            return new Result<T> (false, new[] { error }, messages ?? Enumerable.Empty<string>());
         }
             
-        public static Result<T> Failure(IReadOnlyCollection<ErrorCode> errors)
+        public static Result<T> Failure(IReadOnlyCollection<ErrorCode> errors, IEnumerable<string>? messages = null)
         {
-            return new Result<T>(false, errors);
+            return new Result<T>(false, errors, messages ?? Enumerable.Empty<string>());
         }
     }
 }
