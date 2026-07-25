c5df1151541ffa7a7c1522fd06c24d236dd253df
kirill <kirillchizh2411@gmail.com>
2026-07-25 17:41:59 +0300

    feat(dal): add ITagRepository interface for Tag entity

---
+++ src/NoteVault.DAL/Interfaces/ITagRepository.cs
@@ -0,0 +1,12 @@
+﻿using NoteVault.DAL.Entities;
+
+namespace NoteVault.DAL.Interfaces
+{
+    public interface ITagRepository
+    {
+        Task<List<Tag>> GetAllAsync(CancellationToken cancellationToken = default);
+        Task<Tag?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+        Task<Tag> AddAsync(Tag tag, CancellationToken cancellationToken = default);
+        Task<bool> DeleteAsync(Guid id, CancellationToken cancellationToken = default);
+    }
+}
