10ee8f1ecd07e564266a4377a094b52c0646c829
kirill <kirillchizh2411@gmail.com>
2026-07-25 18:56:48 +0300

    feat(dal): implement TagRepository for database operations

---
+++ src/NoteVault.DAL/Repositories/TagRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NoteVault.DAL.Data;
+using NoteVault.DAL.Entities;
+using NoteVault.DAL.Interfaces;
+
+namespace NoteVault.DAL.Repositories
+{
+    public class TagRepository : ITagRepository
+    {
+        private readonly AppDbContext _context;
+        private readonly DbSet<Tag> _tags;
+
+        public TagRepository(AppDbContext context)
+        {
+            _context = context;
+            _tags = context.Tags;
+        }
+
+        public async Task<List<Tag>> GetAllAsync(CancellationToken cancellationToken = default)
+        {
+            return await _tags
+                .AsNoTracking()
+                .ToListAsync(cancellationToken);
+        }
+
+        public async Task<Tag?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+        {
+            return await _tags
+                .AsNoTracking()
+                .FirstOrDefaultAsync(tag => tag.Id == id, cancellationToken);
+        }
+
+        public async Task<Tag> AddAsync(Tag tag, CancellationToken cancellationToken = default)
+        {
+            await _tags.AddAsync(tag, cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+            return tag;
+        }
+
+        public async Task<bool> DeleteAsync(Guid id, CancellationToken cancellationToken = default)
+        {
+            var deletedCount = await _tags
+                .Where(tag => tag.Id == id)
+                .ExecuteDeleteAsync(cancellationToken);
+
+            return deletedCount != default;
+        }
+    }
+}
