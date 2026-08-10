f6fd46a7d5a29aa54890fd7c8244cd62496f2d64
kirill <kirillchizh2411@gmail.com>
2026-08-11 01:53:18 +0300

    feat(bll): implement PasswordHasher service with PBKDF2

---
+++ src/NoteVault.BLL/Interfaces/IPasswordHasher.cs
@@ -0,0 +1,8 @@
+﻿namespace NoteVault.BLL.Interfaces
+{
+    public interface IPasswordHasher
+    {
+        string HashPassword(string password);
+        bool Verify(string password, string psswordHash);
+    }
+}
---
+++ src/NoteVault.BLL/Services/PasswordHasher.cs
@@ -0,0 +1,36 @@
+﻿using System.Security.Cryptography;
+
+namespace NoteVault.BLL.Services
+{
+    public class PasswordHasher
+    {
+        private const int SaltSize = 16;
+        private const int KeySize = 32;
+        private const int Iterations = 100000;
+        private static readonly HashAlgorithmName HashAlgorithm = HashAlgorithmName.SHA256;
+
+        public string HashPassword(string password)
+        {
+            var salt = RandomNumberGenerator.GetBytes(SaltSize);
+            var hash = Rfc2898DeriveBytes.Pbkdf2(password, salt, Iterations, HashAlgorithm, KeySize);
+
+            return $"{Convert.ToBase64String(salt)}.{Convert.ToBase64String(hash)}";
+        }
+
+        public bool Verify(string password, string passwordHash)
+        {
+            var parts = passwordHash.Split('.', 2);
+            if (parts.Length != 2)
+            {
+                return false;
+            }
+            
+            var salt = Convert.FromBase64String(parts[0]);
+            var hash = Convert.FromBase64String(parts[1]);
+
+            var inputHash = Rfc2898DeriveBytes.Pbkdf2(password, salt, Iterations, HashAlgorithm, KeySize);
+
+            return CryptographicOperations.FixedTimeEquals(hash, inputHash);
+        }
+    }
+}
