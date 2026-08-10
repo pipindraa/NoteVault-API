b8ba385922d626458dda9456bc06dd3871d16387
kirill <kirillchizh2411@gmail.com>
2026-08-10 20:39:46 +0300

    feat(dal): configure User entity and relationship

--- src/NoteVault.DAL/Data/AppDbContext.cs
+++ src/NoteVault.DAL/Data/AppDbContext.cs
@@ -7,9 +7,21 @@ namespace NoteVault.DAL.Data
     {
         public DbSet<Note> Notes { get; set; }
         public DbSet<Tag> Tags { get; set; }
+        public DbSet<User> Users { get; set; }
 
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Notes)
+                .WithOne(n => n.User)
+                .HasForeignKey(n => n.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
         }
     }
 }
