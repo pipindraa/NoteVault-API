52ca7b9c5cdfab5a1f8c31bbc8121c649713b02c
kirill <kirillchizh2411@gmail.com>
2026-08-11 00:16:16 +0300

    feat(dal): add IUserRepository and UserRepository implementation

---
+++ src/NoteVault.DAL/Interfaces/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using NoteVault.DAL.Entities;
+
+namespace NoteVault.DAL.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+        Task<User?> GetByEmailAsync(string email, CancellationToken cancellationToken = default);
+        Task<User?> GetByUsernameAsync(string username, CancellationToken cancellationToken = default);
+        Task<User> AddAsync(User user, CancellationToken cancellationToken = default);
+    }
+}
