12cf5e5cdef9720d2fdffef4873325318de7328d
kirill <kirillchizh2411@gmail.com>
2026-08-17 19:53:02 +0300

    refactor(bll): move hasher constants to separate file

---
+++ src/NoteVault.BLL/Constants/PasswordHasherConstants.cs
@@ -0,0 +1,25 @@
+﻿namespace NoteVault.BLL.Constants
+{
+    public class PasswordHasherConstants
+    {
+        public const char PhcSeparator = '$';
+        public const int PhcPartsCount = 6;
+        public const int AlgorithmIndex = 1;   
+        public const int SubAlgorithmIndex = 2;
+        public const int ParamsIndex = 3;
+        public const int SaltIndex = 4;
+        public const int HashIndex = 5;
+
+        public const string AlgorithmId = "pbkdf2";
+        public const string SubAlgorithmId = "sha256";
+
+        public const char ParamSeparator = ',';
+        public const char KeyValueSeparator = '=';
+        public const int ParamPartsCount = 2;
+        public const int KeyIndex = 0;
+        public const int ValueIndex = 1;
+
+        public const string IterationsParamName = "i";
+        public const string KeySizeParamName = "l";
+    }
+}
--- src/NoteVault.BLL/Services/PasswordHasher.cs
+++ src/NoteVault.BLL/Services/PasswordHasher.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Options;
 using NoteVault.BLL.Common;
+using NoteVault.BLL.Constants;
 using NoteVault.BLL.Interfaces;
 using System.Security.Cryptography;
 
@@ -8,28 +9,7 @@ namespace NoteVault.BLL.Services
     public class PasswordHasher : IPasswordHasher
     {
         private static readonly HashAlgorithmName HashAlgorithm = HashAlgorithmName.SHA256;
-
-        private const char PhcSeparator = '$';
-        private const int PhcPartsCount = 6;
-        private const int AlgorithmIndex = 1;
-        private const int SubAlgorithmIndex = 2;
-        private const int ParamsIndex = 3;
-        private const int SaltIndex = 4;
-        private const int HashIndex = 5;
-
-        private const string AlgorithmId = "pbkdf2";
-        private const string SubAlgorithmId = "sha256";
-
         private readonly PasswordHashingOptions _options;
-
-        private const char ParamSeparator = ',';
-        private const char KeyValueSeparator = '=';
-        private const int ParamPartsCount = 2;
-        private const int KeyIndex = 0;
-        private const int ValueIndex = 1;
-
-        private const string IterationsParamName = "i";
-        private const string KeySizeParamName = "l";
 
         public PasswordHasher(IOptions<PasswordHashingOptions> options)
         {
@@ -41,48 +21,48 @@ namespace NoteVault.BLL.Services
             var salt = RandomNumberGenerator.GetBytes(_options.SaltSize);
             var hash = Rfc2898DeriveBytes.Pbkdf2(password, salt, _options.Iterations, HashAlgorithm, _options.KeySize);
 
-            return $"${AlgorithmId}${SubAlgorithmId}${IterationsParamName}={_options.Iterations},{KeySizeParamName}={_options.KeySize}${Convert.ToBase64String(salt)}${Convert.ToBase64String(hash)}";
+            return $"${PasswordHasherConstants.AlgorithmId}${PasswordHasherConstants.SubAlgorithmId}${PasswordHasherConstants.IterationsParamName}={_options.Iterations},{PasswordHasherConstants.KeySizeParamName}={_options.KeySize}${Convert.ToBase64String(salt)}${Convert.ToBase64String(hash)}";
         }
 
         public bool Verify(string password, string passwordHash)
         {
-            var parts = passwordHash.Split(PhcSeparator);
-            if (parts.Length != PhcPartsCount)
+            var parts = passwordHash.Split(PasswordHasherConstants.PhcSeparator);
+            if (parts.Length != PasswordHasherConstants.PhcPartsCount)
             {
                 return false;
             }
 
-            if (parts[AlgorithmIndex] != AlgorithmId || parts[SubAlgorithmIndex] != SubAlgorithmId)
+            if (parts[PasswordHasherConstants.AlgorithmIndex] != PasswordHasherConstants.AlgorithmId || parts[PasswordHasherConstants.SubAlgorithmIndex] != PasswordHasherConstants.SubAlgorithmId)
             {
                 return false;
             }
 
-            var paramsPart = parts[ParamsIndex].Split(ParamSeparator);
+            var paramsPart = parts[PasswordHasherConstants.ParamsIndex].Split(PasswordHasherConstants.ParamSeparator);
             int iterations = _options.Iterations;
             int keySize = _options.KeySize;
 
             foreach (var param in paramsPart)
             {
-                var keyValue = param.Split(KeyValueSeparator);
-                if (keyValue.Length != ParamPartsCount)
+                var keyValue = param.Split(PasswordHasherConstants.KeyValueSeparator);
+                if (keyValue.Length != PasswordHasherConstants.ParamPartsCount)
                 {
                     continue;
                 }
 
-                if (keyValue[KeyIndex] == IterationsParamName && int.TryParse(keyValue[ValueIndex], out var parsedIterations))
+                if (keyValue[PasswordHasherConstants.KeyIndex] == PasswordHasherConstants.IterationsParamName && int.TryParse(keyValue[PasswordHasherConstants.ValueIndex], out var parsedIterations))
                 {
                     iterations = parsedIterations;
                 }
-                else if (keyValue[KeyIndex] == KeySizeParamName && int.TryParse(keyValue[ValueIndex], out var parsedKeySize))
+                else if (keyValue[PasswordHasherConstants.KeyIndex] == PasswordHasherConstants.KeySizeParamName && int.TryParse(keyValue[PasswordHasherConstants.ValueIndex], out var parsedKeySize))
                 {
                     keySize = parsedKeySize;
                 }
             }
             
-            var salt = Convert.FromBase64String(parts[SaltIndex]);
-            var hash = Convert.FromBase64String(parts[HashIndex]);
+            var salt = Convert.FromBase64String(parts[PasswordHasherConstants.SaltIndex]);
+            var hash = Convert.FromBase64String(parts[PasswordHasherConstants.HashIndex]);
 
-            var inputHash = Rfc2898DeriveBytes.Pbkdf2(password, salt, _options.Iterations, HashAlgorithm, _options.KeySize);
+            var inputHash = Rfc2898DeriveBytes.Pbkdf2(password, salt, iterations, HashAlgorithm, keySize);
 
             return CryptographicOperations.FixedTimeEquals(hash, inputHash);
         }
