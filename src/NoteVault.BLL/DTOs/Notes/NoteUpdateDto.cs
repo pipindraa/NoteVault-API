c639e9af93b092dd8925a88df5b8b2d772c32073
kirill <kirillchizh2411@gmail.com>
2026-08-22 20:48:22 +0300

    fix(bll): restore Required validation attributes

--- src/NoteVault.BLL/DTOs/Notes/NoteUpdateDto.cs
+++ src/NoteVault.BLL/DTOs/Notes/NoteUpdateDto.cs
@@ -1,8 +1,12 @@
-﻿namespace NoteVault.BLL.DTOs.Notes
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NoteVault.BLL.DTOs.Notes
 {
     public class NoteUpdateDto
     {
+        [Required]
         public string Name { get; init; } = string.Empty;
+        [Required]
         public string Description { get; init; } = string.Empty;
         public IReadOnlyCollection<string> ImageUrls { get; init; } = Array.Empty<string>();
         public IReadOnlyCollection<Guid> TagIds { get; init; } = Array.Empty<Guid>();
